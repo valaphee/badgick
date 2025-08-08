04bab2bab88e2e4a3368fb28a2c0b4da782c4811
Valaphee <32491319+valaphee@users.noreply.github.com>
2025-08-08 21:54:26 +0200

    generate pac, implement hal

--- build.rs
+++ build.rs
@@ -4,8 +4,6 @@ fn main() -> io::Result<()> {
     let out_dir = path::PathBuf::from(env::var("OUT_DIR").unwrap());
     println!("cargo:rustc-link-search={}", out_dir.display());
 
-    //fs::write(out_dir.join("device.x"), include_bytes!("device.x"))?;
-    println!("cargo:rerun-if-changed=device.x");
     fs::write(out_dir.join("memory.x"), include_bytes!("memory.x"))?;
     println!("cargo:rerun-if-changed=memory.x");
     println!("cargo:rustc-link-arg=-Tlink.x");
